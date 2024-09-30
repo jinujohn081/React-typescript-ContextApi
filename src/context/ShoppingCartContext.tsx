5efb6fe1af3b7b64ed586daeb6996680dabb2829
Jinu <jinujohn081@gmail.com>
2024-09-30 10:44:20 +0200

    first commit

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,24 @@
+
+import {createContext,useContext} from 'react'
+type ShoppingCartProviderProps={
+ children:ReactNode
+}
+type ShoppingCartContext={
+ getItemQuantity:(id:number)=>number
+  increaseCartQuantity:(id:number)=>void
+   decreaseCartQuantity:(id:number)=>void
+    removeFromCart:(id:number)=>void
+}
+const ShoppingCartContext=createContext({} as ShoppingCartContext)
+export function useShoppingCart(){
+ return useContext(ShoppingCartContext)
+}
+
+
+export function ShoppingCartProvider({children}:ShoppingCartProvider){
+ return(
+  <ShoppingCartContext.Provider value={{}}>
+   {children}
+   </ShoppingCartContext.Provider>
+ )
+}
