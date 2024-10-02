8e7e138cfad8d34dbe4202d92ebd36cba647d5ec
jinu <jinujohn081@gmail.com>
2024-10-02 15:38:30 +0200

    modified the other components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { Navbar as NavbarBs, Container, Nav, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { useShoppingCart } from '../context/ShoppingCartContext'
 
 const Navbar = () => {
+  const {openCart,cartQuantity}=useShoppingCart()
   return (
     <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
       <Container>
@@ -16,7 +18,8 @@ const Navbar = () => {
             About
           </Nav.Link>
         </Nav>
-        <Button
+       {cartQuantity > 0 && ( <Button
+        onClick={openCart}
           style={{ width: '3rem', height: '3rem', position: 'relative' }}
           variant='outline-primary'
           className='rounded-circle'
@@ -40,9 +43,9 @@ const Navbar = () => {
               transform: 'translate(25% 25%)',
             }}
           >
-            3
+            {cartQuantity}
           </div>
-        </Button>
+        </Button>)}
       </Container>
     </NavbarBs>
   )
